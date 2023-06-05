45bf63ffe1cd2d896a3c7557d9d6d8e71cfbd961
dhinadk28 <103361324+dhinadk28@users.noreply.github.com>
2023-06-05 14:40:19 +0530

    add

---
+++ src/components/Homepage/Navbar/Navbar.jsx
@@ -0,0 +1,95 @@
+import { ReactNode } from 'react';
+import {
+  Box,
+  Flex,
+  Avatar,
+  Link,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+} from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { LOGOUT } from '../../../Redux/users/user.types';
+
+export default function Navbar() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const dispatch = useDispatch()
+  const {auth,token,loading,error} = useSelector((state)=>state.userReducer)
+  console.log(auth)
+
+  const nav = useNavigate()
+  return (
+    <>
+      <Box zIndex={1000} position={"fixed"} top={0} w={"100%"}  boxShadow={"rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;"}  bg={"#90CAF9"} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <Box fontWeight={"bold"} cursor={"pointer"} onClick={()=>{
+            nav("/")
+          }} color="white">MindMemo</Box>
+
+          <Flex alignItems={'center'}>
+            <Stack alignItems={"center"} direction={'row'} spacing={7}>
+                <Button display={auth==true?"block":"none"}  bg={"blue"}m color={"#90CAF9"} onClick={()=>{
+                    nav("/notes")
+                }}>All Notes</Button>
+                <Button display={auth==true?"none":"block"}  bg={"blue"}m color={"#90CAF9"} onClick={()=>{
+                    nav("/register")
+                }}>Sign up</Button>
+                <Button display={auth==true?"none":"block"} bg={"blue"}m color={"#90CAF9"} onClick={()=>{
+                    nav("/login")
+                }}>Login</Button>
+              <Button bg={"white"} onClick={toggleColorMode}>
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              </Button>
+
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  border="2px solid blue"
+                  padding={2}
+                  rounded={'full'}
+                  variant={'link'}
+                  cursor={'pointer'}
+                  minW={0}>
+                  <Avatar
+                    size={'sm'}
+                    src={'https://api.dicebear.com/6.x/avataaars/svg'}
+                  />
+                </MenuButton>
+                <MenuList alignItems={'center'}>
+                  <br />
+                  <Center>
+                    <Avatar
+                      size={'2xl'}
+                      src={'https://api.dicebear.com/6.x/avataaars/svg'}
+                    />
+                  </Center>
+                  <br />
+                  <Center>
+                    <p>Username</p>
+                  </Center>
+                  <br />
+                  <MenuDivider />
+                
+                  <MenuItem onClick={()=>{
+                    dispatch({type:LOGOUT})
+                  }}>Logout</MenuItem>
+                </MenuList>
+              </Menu>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
